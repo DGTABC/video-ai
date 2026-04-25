246e4f78c6a4b73ee1c6bc9efb938a4c4ff9b988
DGTABC <wkwntmsmsanfrjs@gmail.com>
2026-04-26 03:43:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def process():
         "result": response['choices'][0]['message']['content']
     })
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
